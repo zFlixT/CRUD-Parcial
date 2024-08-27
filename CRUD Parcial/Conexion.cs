13bfa3c678913a269500e131509e8043f5c1f4e3
zFlixT <pt0109032023@unab.edu.sv>
2024-08-27 11:44:18 -0600

    Se creo el nuevo formulario con el formato detalles

---
+++ CRUD Parcial/Conexion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUD_Parcial
+{
+    public partial class Conexion : Form
+    {
+        public Conexion()
+        {
+            InitializeComponent();
+        }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void customersBindingNavigatorSaveItem_Click_1(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void Conexion_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwindDataSet.Customers' Puede moverla o quitarla según sea necesario.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
+    }
+}

998619d45e60d0d7680548f8016e84bd8b368d76
zFlixT <pt0109032023@unab.edu.sv>
2024-08-27 11:32:35 -0600

    Se agrego la tabla Customers junto con el CRUD

--- CRUD Parcial/Form1.cs
+++ CRUD Parcial/Form1.cs
@@ -16,5 +16,20 @@ namespace CRUD_Parcial
         {
             InitializeComponent();
         }
+
+        private void customersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.customersBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwindDataSet);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwindDataSet.Customers' Puede moverla o quitarla según sea necesario.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
     }
 }

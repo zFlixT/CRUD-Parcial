5b54ab536fde45ec1615f0b2f39a5581f4f1a9be
zFlixT <pt0109032023@unab.edu.sv>
2024-08-27 11:14:50 -0600

    Coneccion de la base de datos Northwind con el proyecto

--- CRUD Parcial/Program.cs
+++ CRUD Parcial/Program.cs
@@ -17,6 +17,7 @@ namespace CRUD_Parcial
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
         }
     }
 }
